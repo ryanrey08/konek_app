aa48d8cac19f7305a58fc9dc6dd0125698d49205
Ryan Rey Foster <rfoster@ip-192-168-1-10.ap-northeast-1.compute.internal>
2024-08-27 13:24:52 +0800

    update endpoint

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -10,15 +10,15 @@ import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 
 //my local
 //final String pre_url = "http://192.168.42.88:8000";
-const String pre_url = "https://113.19.24.196:443/api/v1/auth";
-const String pre_url_voucher = "https://113.19.24.196:443/api/v1/voucher/";
-const String pre_url_subscription = "https://113.19.24.196:443/api/v1/subscriptions";
-const String pre_url_ads = "https://113.19.24.196:443/api/v1/quick-links-and-ads";
+const String pre_url = "https://1konek.swakwifi.com/api/v1/auth";
+const String pre_url_voucher = "https://1konek.swakwifi.com/api/v1/voucher/";
+const String pre_url_subscription = "https://1konek.swakwifi.com/api/v1/subscriptions";
+const String pre_url_ads = "https://1konek.swakwifi.com/api/v1/quick-links-and-ads";
 const String img_url = "http://192.168.42.88:8000/storage/";
-const String file_storage = "https://113.19.24.196:443/storage/";
-const String hit_pay = "https://113.19.24.196:443/api/v1/hitpay/";
-const String my_address = "https://113.19.24.196:443/api/v1/utilities/";
-const String auth_update_profile = "https://113.19.24.196:443/api/v1/account/";
+const String file_storage = "https://1konek.swakwifi.com/storage/";
+const String hit_pay = "https://1konek.swakwifi.com/api/v1/hitpay/";
+const String my_address = "https://1konek.swakwifi.com/api/v1/utilities/";
+const String auth_update_profile = "https://1konek.swakwifi.com/api/v1/account/";
 
 
 String throwErrorAuth(String error) {
